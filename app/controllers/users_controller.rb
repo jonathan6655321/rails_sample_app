5531d3c43f4a11882d8c79cd96cb9ec15cd4d920
Jonathan Somer <jonathan.somer@fiverr.com>
2017-09-27 16:59:37 +0300

    add log-in prompt for edit/update requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,6 +20,21 @@ class UsersController < ApplicationController
     end
   end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
@@ -25,4 +42,10 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
---
+++ summary.md
@@ -0,0 +1,4 @@
+#Chapter 10:
+
+
+* When constructing a form using form_for(@user), Rails uses POST if @user.new_record? is true and PATCH if it is false.
